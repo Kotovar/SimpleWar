50399fe7d7587e4a082ce0d32ba0794c060f25ed
dmitrii <neasit3@gmail.com>
2026-09-23 15:00:24 +0700

    chore: мигрцаия на vite+

--- src/entities/maps/model/generateMap.test.ts
+++ src/entities/maps/model/generateMap.test.ts
@@ -1,4 +1,4 @@
-import { afterEach, expect, it, vi } from 'vitest';
+import { afterEach, expect, it, vi } from 'vite-plus/test';
 import { generateMap } from './generateMap';
 
 afterEach(() => vi.restoreAllMocks());
